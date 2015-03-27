0285c983275126686410bc0c4482fb5072a16f83
Jon Horner <hornerjn@mail.uc.edu>
2015-03-27 12:31:36 -0400

    got rename and transpile working

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
+var path = require('path');
+
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var del = require('del');
+var rename = require('gulp-rename');
 
-gulp.task('default', ['scripts'], function() {
+gulp.task('default', ['babel'], function() {
 
 });
 
 gulp.task('clean', function(cb) {
-  del(['index.js'], cb);
+  del(['generated'], cb);
 });
 
 gulp.task('scripts', ['clean'], function() {
@@ -16,4 +19,18 @@ gulp.task('scripts', ['clean'], function() {
     .pipe(babel())
     .pipe(concat('index.js'))
     .pipe(gulp.dest('.'));
+});
+
+gulp.task('changeExt', ['clean'], function() {
+  return gulp.src('./lib/**/*.es6')
+    .pipe(rename(function(srcpath) {
+      srcpath.extname = '.js';
+    }))
+    .pipe(gulp.dest('./generated/renamed'));
+});
+
+gulp.task('babel', ['changeExt'], function() {
+  return gulp.src('./generated/renamed/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./generated/transpiled'));
 });
