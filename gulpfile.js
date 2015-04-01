d739075afeab6b2ba1612541817cc8c60cd09811
Jon Horner <hornerjn@mail.uc.edu>
2015-04-01 10:00:10 -0400

    removed grunt

--- gulpfile.js
+++ gulpfile.js
@@ -11,23 +11,15 @@ function changedJsWarning(event) {
   console.warn('\'./lib/js/\' is a generated directory. Changes made here are liable to be overwritten.');
 }
 
-gulp.task('watch', ['buildEs6', 'watchEs6', 'watchJs']);
+gulp.task('default', ['cleanSlate', 'generateJs']);
 
-gulp.task('watchEs6', ['buildEs6'], function() {
-  gulp.watch('./lib/es6/**/*.es6', ['buildEs6']);
+gulp.task('watch', ['cleanSlate', 'generateJs', 'watchEs6', 'watchJs']);
+
+gulp.task('cleanSlate', function() {
+  del(['./lib/js/**/*.js']);
 });
 
-gulp.task('watchJs', function() {
-  var watcher = gulp.watch('./lib/js/**/*.js');
-  var cb = function(event) {
-    if (!fs.existsSync('./generated/changedEs6.txt')) {
-      changedJsWarning(event);
-    }
-  };
-  watcher.on('change', cb);
-});
-
-gulp.task('buildEs6', ['setChangedEs6', 'transpileEs6', 'cleanup']);
+gulp.task('generateJs', ['cleanSlate', 'setChangedEs6', 'transpileEs6', 'cleanup']);
 
 gulp.task('setChangedEs6', function() {
   if (!fs.existsSync('./generated')) {
@@ -36,13 +28,13 @@ gulp.task('setChangedEs6', function() {
   fs.writeFileSync('./generated/changedEs6.txt', 'true');
 });
 
-gulp.task('transpileEs6', ['setChangedEs6', 'changeExt'], function() {
+gulp.task('transpileEs6', ['cleanSlate', 'setChangedEs6', 'changeExtension'], function() {
   return gulp.src('./generated/renamed/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./lib/js'));
 });
 
-gulp.task('changeExt', function() {
+gulp.task('changeExtension', function() {
   return gulp.src('./lib/es6/**/*.es6')
     .pipe(rename(function(srcpath) {
       srcpath.extname = '.js';
@@ -56,38 +48,16 @@ gulp.task('cleanup', ['setChangedEs6', 'transpileEs6'], function(cb) {
   }, 1000);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-gulp.task('default', ['buildEs6']);
-
-
-
-gulp.task('changedEs6', function(cb) {
-  if (!fs.existsSync('./generated')) {
-    fs.mkdirSync('./generated');
-  }
-  fs.writeFileSync('./generated/changedEs6.txt', 'true');
+gulp.task('watchEs6', ['generateJs'], function() {
+  gulp.watch('./lib/es6/**/*.es6', ['generateJs']);
 });
 
-// gulp.task('scripts', ['clean'], function() {
-//   return gulp.src('./lib/**/*.js')
-//     .pipe(babel())
-//     .pipe(concat('index.js'))
-//     .pipe(gulp.dest('.'));
-// });
+gulp.task('watchJs', function() {
+  var watcher = gulp.watch('./lib/js/**/*.js');
+  var cb = function(event) {
+    if (!fs.existsSync('./generated/changedEs6.txt')) {
+      changedJsWarning(event);
+    }
+  };
+  watcher.on('change', cb);
+});
